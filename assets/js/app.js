b07d734759ad491ca3a522498f7d75ee4c146587
Angel Torres <angtor.dev@gmail.com>
2022-12-29 14:20:09 -0400

    added test notifications

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 console.log("Hello World")
+
+document.getElementById('notifications').addEventListener("click", () => {
+    Notification.requestPermission().then(result => {
+        if (result == "granted") {
+            console.log("Permiso otorgado")
+            randomNotification();
+        }
+    })
+})
+
+function randomNotification() {
+    let notifTitle = "Este es el titulo"
+    let notifBody = "Contrenido de la notificación"
+    let notifImg = "assets/img/favicon.png"
+    let options = {
+        body: notifBody,
+        icon: notifImg,
+    }
+
+    new Notification(notifTitle, options)
+}
